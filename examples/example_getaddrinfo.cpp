0498cbad6784c81f2a0102a08c31e801d9e77b2b
TheClonerx <theclonerx@outlook.com>
2020-05-11 16:06:49 -0400

    Prittier addresses for getaddrinfo example

--- examples/example_getaddrinfo.cpp
+++ examples/example_getaddrinfo.cpp
@@ -5,7 +5,13 @@
 #include <cppnet/getaddrinfo.hpp>
 #include <arpa/inet.h>
 
-std::string addr_to_str(const sockaddr* addr); // browser friendly address
+std::string addr_to_str(const sockaddr_storage* addr); // browser friendly address
+
+std::ostream& operator<<(std::ostream& os, net::address const& addr)
+{
+    os << addr_to_str(addr.address_pointer());
+    return os;
+}
 
 int main(int argc, const char* argv[]) try {
     if (argc < 2 || argc > 3) {
@@ -24,31 +30,41 @@ int main(int argc, const char* argv[]) try {
         std::cout << "\tFamily:   " << ainfo.family()
                   << "\tType:     " << ainfo.type()
                   << "\tProtocol: " << ainfo.protocol()
-                  << "\tAddress:  " << addr_to_str(ainfo.address().address_pointer());
+                  << "\tAddress:  " << ainfo.address();
 
         if (!ainfo.canon_name().empty())
             std::cout << "\tCanon name: " << ainfo.canon_name();
         std::cout << '\n';
     }
 } catch (std::system_error& e) {
-    std::cerr << e.code().category().name()
+    std::cerr << '\n' << e.code().category().name()
               << " error (" << e.code().value() << "):\n\t"
               << e.what() << '\n';
 } catch (std::exception& e) {
-    std::cerr << "std exception:\n\t" << e.what() << '\n';
+    std::cerr << "\nstd exception:\n\t" << e.what() << '\n';
 }
 
-std::string addr_to_str(const sockaddr* addr) // browser friendly address
+std::string addr_to_str(const sockaddr_storage* addr) // browser friendly address
 {
-    char buffer[64] { 0 };
-    if (addr->sa_family == AF_INET) {
+    using namespace std::literals;
+    std::string buffer(64, '\0');
+    if (addr->ss_family == AF_INET) {
         auto in_addr = reinterpret_cast<const sockaddr_in*>(addr);
-        inet_ntop(AF_INET, &in_addr->sin_addr, buffer, std::size(buffer));
-        return buffer + (':' + std::to_string(ntohs(in_addr->sin_port)));
-    } else if (addr->sa_family == AF_INET6) {
+        inet_ntop(AF_INET, &in_addr->sin_addr, std::data(buffer), std::size(buffer));
+    } else if (addr->ss_family == AF_INET6) {
         auto in6_addr = reinterpret_cast<const sockaddr_in6*>(addr);
-        inet_ntop(AF_INET6, &in6_addr->sin6_addr, buffer, std::size(buffer));
-        return "[" + (buffer + ("]:" + std::to_string(ntohs(in6_addr->sin6_port))));
+        inet_ntop(AF_INET6, &in6_addr->sin6_addr, std::data(buffer), std::size(buffer));
+    } else {
+        buffer = "???";
     }
-    return "???";
+    buffer.resize(std::strlen(buffer.c_str()));
+    if (auto in_addr = reinterpret_cast<sockaddr_in const*>(addr); in_addr->sin_port) {
+        if (addr->ss_family == AF_INET6) {
+            buffer = "["s + buffer;
+            buffer += "]"sv;
+        }
+        buffer += ":"sv;
+        buffer += std::to_string(htons(in_addr->sin_port));
+    }
+    return buffer;
 }
