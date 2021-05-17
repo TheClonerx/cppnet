b9138a72a49d9def0a872ce2fec1e745ce92d038
TheClonerx <theclonerx@outlook.com>
2021-05-17 00:50:44 -0400

    Removed unnecessary function overrides for addrinfo_category

--- src/getaddrinfo.cpp
+++ src/getaddrinfo.cpp
@@ -11,13 +11,13 @@
 const std::error_category& net::addrinfo_category() noexcept
 {
     class addrinfo_category_t : public std::error_category {
-        // using std::error_category::error_category;
     public:
         const char* name() const noexcept override
         {
             static const char val[] = "addrinfo";
             return val;
         }
+
         std::string message(int ecode) const override
         {
 #ifdef _WIN32
@@ -25,18 +25,6 @@ const std::error_category& net::addrinfo_category() noexcept
 #else
             return gai_strerror(ecode);
 #endif
-        }
-        bool equivalent(int ecode, std::error_condition const& econd) const noexcept override
-        {
-            return econd.category() == *this && ecode == econd.value();
-        }
-        bool equivalent(std::error_code const& ecode, int econd) const noexcept override
-        {
-            return ecode.category() == *this && ecode.value() == econd;
-        }
-        std::error_condition default_error_condition(int ecode) const noexcept override
-        {
-            return std::error_condition(ecode, *this);
         }
     };
 
